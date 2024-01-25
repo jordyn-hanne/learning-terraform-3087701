033c62254eeb982d3025d811a9255a637350ba97
Jordyn Hanne <39203322+jordyn-hanne@users.noreply.github.com>
2024-01-25 12:29:29 -0800

    update vpc

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in. Allow everything out."
-  vpc = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
